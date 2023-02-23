cc20a6437a358f5c4da67755d07f085247e50c6d
zhangyue <zhangyuea@hualala.com>
2023-02-23 18:51:08 +0800

    feat#完成server1

---
+++ server1.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use((request, response, next) => {
+    console.log('有人请求服务器1了');
+    next();
+});
+
+app.get('/students', (request, response) => {
+    const students = [
+        {id: '001', name: 'tom', age: 18},
+        {id: '002', name: 'jerry', age: 19},
+        {id: '003', name: 'tony', age: 20},
+    ];
+    response.send(students);
+});
+
+app.listen(5000, (err) => {
+    if(!err) console.log('服务器1启动成功了，请求学生信息地址为：http://localhost:5000/students');
+})
